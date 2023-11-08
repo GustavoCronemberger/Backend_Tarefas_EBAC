13b6235a37cbe76ab22951f99f965f04515ecb2e
Gustavo Cronemberger <gustavocronemberger@gmail.com>
2023-11-08 17:00:41 -0300

    Segundo

---
+++ src/main/Lista.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Lista {
+    public static void main (String[] args){
+        listaNomesOrdemAlfabetica();
+        
+        
+    }
+
+    private static void listaNomesOrdemAlfabetica() {
+        System.out.println("***ListaOrdemAlfabética***");
+    }
+}
