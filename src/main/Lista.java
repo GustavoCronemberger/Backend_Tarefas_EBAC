cb4727bc7ee4a972296023a917221f90a995569e
Gustavo Cronemberger <gustavocronemberger@gmail.com>
2023-11-08 20:02:16 -0300

    Final

--- src/main/Lista.java
+++ src/main/Lista.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Lista {
     public static void main (String[] args){
         listaNomesOrdemAlfabetica();
@@ -9,5 +13,14 @@ public class Lista {
 
     private static void listaNomesOrdemAlfabetica() {
         System.out.println("****ListaOrdemAlfabética****");
+        List<String> lista = new ArrayList<>();
+        lista.add("Fabrício");
+        lista.add("Leonardo");
+        lista.add("Gustavo");
+        lista.add("Felipe");
+        Collections.sort(lista);
+        System.out.println(lista);
+        System.out.println(" ");
+
     }
 }
